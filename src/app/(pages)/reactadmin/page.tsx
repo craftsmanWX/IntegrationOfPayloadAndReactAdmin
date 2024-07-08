b29515d76f95cfc19908d6981530ddfdab421e3b
weixu <weixu.craftsman@gmail.com>
2024-07-08 17:59:33 +1000

    add a App page

---
+++ src/app/(pages)/reactadmin/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react';
+
+export type CartContext = {
+  // Define the properties of CartContext here
+}
+
+const Reactadmin: React.FC = () => {
+  return (
+    <div>Reactadmin</div>
+  );
+}
+
+export default Reactadmin;
