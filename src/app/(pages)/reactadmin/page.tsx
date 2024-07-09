4ffe8570875f57ae669b5fa5a0ed5712f824fab0
weixu <weixu.craftsman@gmail.com>
2024-07-09 11:31:19 +1000

    Integrate react-admin UI by removing header and footer

--- src/app/(pages)/reactadmin/page.tsx
+++ src/app/(pages)/reactadmin/page.tsx
@@ -1,15 +1,26 @@
-'use client'
+// in src/components/AdminApp.tsx
+"use client"; // remove this line if you choose Pages Router
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
 
-import React from 'react';
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-export type CartContext = {
-  // Define the properties of CartContext here
-}
+const AdminApp = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="name"
+    />
+    <Resource
+      name="posts"
+      list={ListGuesser}
+      edit={EditGuesser}
+      recordRepresentation="title"
+    />
+    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+  </Admin>
+);
 
-const Reactadmin: React.FC = () => {
-  return (
-    <div>Reactadmin</div>
-  );
-}
-
-export default Reactadmin;
+export default AdminApp;
